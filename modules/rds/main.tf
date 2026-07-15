e51d8b0c4a2c5c5f2a04ecffb02feccfc0e077f5
kodegravity <info@kodegravity.com>
2026-07-14 21:32:51 -0400

    commented rds code

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -14,42 +14,42 @@ resource "aws_db_subnet_group" "main" {
   })
 }
 
-resource "aws_db_instance" "main" {
-  identifier = "${var.project}-${var.environment}-postgres"
-
-  engine         = "postgres"
-  engine_version = var.postgres_version
-  instance_class = var.instance_class
-
-  allocated_storage     = var.allocated_storage
-  max_allocated_storage = var.allocated_storage * 2
-  storage_type          = "gp2"
-  storage_encrypted     = true
-
-  db_name  = var.db_name
-  username = var.db_username
-  password = random_password.db_password.result
-
-  db_subnet_group_name   = aws_db_subnet_group.main.name
-  vpc_security_group_ids = var.security_group_ids
-
-  multi_az            = var.multi_az
-  publicly_accessible = false
-
-  backup_retention_period = 1
-  backup_window           = "03:00-04:00"
-  maintenance_window      = "Mon:04:00-Mon:05:00"
-
-  skip_final_snapshot = var.skip_final_snapshot
-  deletion_protection = var.deletion_protection
-
-  performance_insights_enabled = false
-  monitoring_interval          = 0
-
-  tags = merge(var.tags, {
-    Name = "${var.project}-${var.environment}-postgres"
-  })
-}
+# resource "aws_db_instance" "main" {
+#   identifier = "${var.project}-${var.environment}-postgres"
+#
+#   engine         = "postgres"
+#   engine_version = var.postgres_version
+#   instance_class = var.instance_class
+#
+#   allocated_storage     = var.allocated_storage
+#   max_allocated_storage = var.allocated_storage * 2
+#   storage_type          = "gp2"
+#   storage_encrypted     = true
+#
+#   db_name  = var.db_name
+#   username = var.db_username
+#   password = random_password.db_password.result
+#
+#   db_subnet_group_name   = aws_db_subnet_group.main.name
+#   vpc_security_group_ids = var.security_group_ids
+#
+#   multi_az            = var.multi_az
+#   publicly_accessible = false
+#
+#   backup_retention_period = 1
+#   backup_window           = "03:00-04:00"
+#   maintenance_window      = "Mon:04:00-Mon:05:00"
+#
+#   skip_final_snapshot = var.skip_final_snapshot
+#   deletion_protection = var.deletion_protection
+#
+#   performance_insights_enabled = false
+#   monitoring_interval          = 0
+#
+#   tags = merge(var.tags, {
+#     Name = "${var.project}-${var.environment}-postgres"
+#   })
+# }
 
 # Store all DB credentials in a single Secrets Manager secret as JSON
 resource "aws_secretsmanager_secret" "db_credentials" {
